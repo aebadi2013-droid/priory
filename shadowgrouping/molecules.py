da20fd858f6581d854e96792318412e1bbd50bc4
Alexander Gresch <alexander.gresch@hhu.de>
2023-08-28 17:35:50 +0200

    added Fe2S2 and some decor

--- shadowgrouping/molecules.py
+++ shadowgrouping/molecules.py
@@ -4,6 +4,8 @@ from qiskit_nature.drivers import Molecule
 import numpy as np
 
 available_molecules = ["H2","H2_6-31g","LiH","BeH2","H2O","NH3"]
+available_molecules_latex = ["\ce{H2}","\ce{H2} (6-31g)","\ce{LiH}","\ce{BeH2}","\ce{H2O}","\ce{NH3}"]
+available_molecules_E_GS = [-1.86,-1.86,-8.91,-19.05,-83.60,-66.88]
 CHEMICAL_ACCURACY = 1.6e-3
 
 # source: http://hyperphysics.phy-astr.gsu.edu/hbase/Tables/diatomic.html
@@ -41,7 +43,7 @@ H2O = Molecule(geometry=[['O', [0., 0., 0.]],
 alpha = np.deg2rad(106.7) # angle between any two H atoms
 R = 1.012 # equilibrium bond distance between N and any H
 beta = np.deg2rad(60) # planar angle of the base triangle spanned by all H
-# gemeometry considerations:
+# geometry considerations:
 d = R*np.sin(alpha/2)/np.sin(beta)
 x = np.sin(beta/2)*d
 y = np.cos(beta/2)*d
@@ -59,3 +61,13 @@ NH3 = Molecule(geometry=[['N', [0., 0., H]],
 N2 = Molecule(geometry=[['N', [0., 0., 0.]],
                               ['N', [0., 0., 1.1]]],
                      charge=0, multiplicity=1)
+
+# source: https://iopscience.iop.org/article/10.1088/0953-4075/43/16/165101
+R_Fe = 0.254
+R_FeS = 0.21
+y_S = np.sqrt(R_Fe**2/4+R_FeS**2)
+Fe2S2 = Molecule(geometry=[['Fe', [R_Fe/2,0,0]],
+                           ['S', [0,y_S,0]],
+                           ['S', [0,-y_S,0]],
+                           ['Fe', [-R_Fe/2,0,0]]],
+                           charge=0, multiplicity=1)
