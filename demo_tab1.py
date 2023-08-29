e46e9b497dc6dd71ca2d1c19e7f01005baafe452
Alexander Gresch <alexander.gresch@hhu.de>
2023-08-29 09:51:29 +0200

    added __main__ part of file

--- demo_tab1.py
+++ demo_tab1.py
@@ -50,82 +50,37 @@ class TexTable():
             rows_std.append(np.array(stds_mol))
         return np.array(rows), np.array(rows_std), np.array(best_method_idxs)
 
-    def plot_values_tex_table(self,outfile):
-        rows, stds, idxs = self.get_rmse_rows()
-        running_ind = 0
-        # put data to file
-        with open(outfile,"w") as f:
-            # header lines
-            f.write("\\begin{tabular}{c|c|"+"c"*len(self.methods)+"}\nMolecule & \multirow{2}{*}{Enc.}")
-            for method in self.methods:
-                f.write(" & " + method)
-            f.write("\\\\ \n")
-            # second header line
-            f.write("($E_\\mathrm{GS}$ [mHa]) & " + "& "*len(self.methods))
-            f.write("\\\\ \\hline\n")
-            # molecule rows        
-            for block, block_stds, molecule, molecule_tag in zip(rows, stds, self.molecules, self.molec_tex):
-                E_GS = self.energies.get(molecule)
-                if E_GS is None:
-                    running_ind += len(self.mappings)
-                    continue
-                first_cols = ["\\multicolumn{1}{c|}{}"]*len(self.mappings)
-                first_cols[0] = first_cols[0][:-1] + "\\multirow{"+ "{}".format(len(self.mappings)) +"}{*}{\\shortstack[c]{" + molecule_tag + "\\\\ {:.2f}".format(E_GS) + "$\\times 10^3$}}}"
-                for row, row_std, start, enc in zip(block, block_stds, first_cols, self.mappings):
-                    f.write(start) # first col
-                    f.write(" & \\multicolumn{1}{c|}{" + enc + "}")
-                    for j,(val,std) in enumerate(zip(row,row_std)):
-                        std = 1000*std if std > 0 else None
-                        rounder = RoundRelevant(1000*val,std)
-                        if j==idxs[running_ind]:
-                            # highlighted value indicating minimum
-                            f.write(rounder.print_val_std(prepend=" & \\textbf{",append="}"))
-                        else:
-                            if np.isinf(val):
-                                f.write(" & ")
-                            else:
-                                f.write(rounder.print_val_std(prepend=" & "))
-                    running_ind += 1
-                    # add \\ command for TeX
-                    if enc == self.mappings[-1]:
-                        f.write(" \\\\ \\hline\n")
-                    else:
-                        f.write(" \\\\\n")
-            # footer lines
-            f.write("\\end{tabular}\n")
-        return
-
-rmse_dict = {}
-std_dict  = {}
-for ind,molecule_name in enumerate(available_molecules):
-    for map_name in mappings.keys():
-        try:
-            temp_dict = load_dict(folder + "{}_molecule_{}_empirical.txt".format(molecule_name,map_name))
-            if ind==0:
-                method_names = list(temp_dict.keys())
-                #method_names = [method for method in method_names if method not in ["AEQuO-inadaptive","ShadowAdaptive"]]
-        except Exception as e:
-            print(e)
-            continue
-        else:
-            for key in method_names:
-                vals = temp_dict.get(key,None)
-                if vals is None:
-                    continue
-                rmse_dict[(molecule_name,map_name,key)] = vals[0]
-                std_dict[(molecule_name,map_name,key)]  = vals[1]
-
-
-energies = {m: available_molecules_E_GS[i] for i,m in enumerate(available_molecules)}
-table = TexTable(rmse_dict,available_molecules,list(mappings.keys()),method_names,energies,available_molecules_latex,std_dict)
-
-for molecule_name in available_molecules:
-    print(molecule_name)
-    for map_name in mappings.keys():
-        print(map_name)
-        for label in table.methods:
-            key = (molecule_name,map_name,label)
-            rmse_data = rmse_dict.get(key,None)
-            if rmse_data is None:
+if __name__=="__main__":
+    rmse_dict = {}
+    std_dict  = {}
+    for ind,molecule_name in enumerate(available_molecules):
+        for map_name in mappings.keys():
+            try:
+                temp_dict = load_dict(folder + "{}_molecule_{}_empirical.txt".format(molecule_name,map_name))
+                if ind==0:
+                    method_names = list(temp_dict.keys())
+            except Exception as e:
+                print(e)
                 continue
-            print(label,np.round(1000*rmse_dict[key],1),"+/-",np.round(1000*std_dict[key],1))
+            else:
+                for key in method_names:
+                    vals = temp_dict.get(key,None)
+                    if vals is None:
+                        continue
+                    rmse_dict[(molecule_name,map_name,key)] = vals[0]
+                    std_dict[(molecule_name,map_name,key)]  = vals[1]
+
+
+    energies = {m: available_molecules_E_GS[i] for i,m in enumerate(available_molecules)}
+    table = TexTable(rmse_dict,available_molecules,list(mappings.keys()),method_names,energies,available_molecules_latex,std_dict)
+
+    for molecule_name in available_molecules:
+        print(molecule_name)
+        for map_name in mappings.keys():
+            print(map_name)
+            for label in table.methods:
+                key = (molecule_name,map_name,label)
+                rmse_data = rmse_dict.get(key,None)
+                if rmse_data is None:
+                    continue
+                print(label,np.round(1000*rmse_dict[key],1),"+/-",np.round(1000*std_dict[key],1))
