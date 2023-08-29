0e77dccc93acf28b314d632caebd349f6934e4be
Alexander Gresch <alexander.gresch@hhu.de>
2023-08-29 09:53:42 +0200

    added __main__ part of file

--- demo_fig2.py
+++ demo_fig2.py
@@ -20,20 +20,21 @@ color_filled = "#E7E1C4"
 color_dotted = "#BCC8B4"
 #######################################################
 
-plt.figure(figsize=(6,3),dpi=400)
-plt.loglog(delta,numerator,color=color_line)
-#plt.text(6e-4,2e3,"$N_\delta$",color="blue",fontsize="x-large")
-plt.fill_between(delta,0.9*n_min,numerator,color=color_filled,alpha=0.4)
-plt.text(1e-8,100,r'$\epsilon_\mathrm{sys} \leq \epsilon_\mathrm{stat}$',fontsize=22)
-for x in np.arange(1,7):
-    sigma = 1-erf(x/np.sqrt(2))
-    plt.vlines(sigma,0.9*n_min,1e4,colors=color_dotted,linestyles="dotted",alpha=0.8)
-    plt.text(0.1*sigma,1.05*n_min,r'${}\sigma$'.format(x),color=color_dotted,fontsize=16)
-plt.xlim(left=min(delta),right=0.5)
-plt.ylim(0.9*min(numerator),1e3)
-plt.xlabel('inconfidence level $\delta$',fontsize="x-large")
-plt.ylabel('collected samples $N_i$',fontsize="x-large")
-plt.xticks([1e-10,1e-8,1e-6,1e-4,1e-2],["$10^{"+str(i)+"}$" for i in np.arange(-10,-1,2)],fontsize="x-large")
-plt.yticks([1e2,1e3],["$10^{"+str(i)+"}$" for i in np.arange(2,4)],fontsize="x-large")
-#plt.minorticks_off()
-plt.savefig("generated_figures/fig2_demo.png",bbox_inches="tight")
+if __name__ == "__main__":
+    plt.figure(figsize=(6,3),dpi=400)
+    plt.loglog(delta,numerator,color=color_line)
+    #plt.text(6e-4,2e3,"$N_\delta$",color="blue",fontsize="x-large")
+    plt.fill_between(delta,0.9*n_min,numerator,color=color_filled,alpha=0.4)
+    plt.text(1e-8,100,r'$\epsilon_\mathrm{sys} \leq \epsilon_\mathrm{stat}$',fontsize=22)
+    for x in np.arange(1,7):
+        sigma = 1-erf(x/np.sqrt(2))
+        plt.vlines(sigma,0.9*n_min,1e4,colors=color_dotted,linestyles="dotted",alpha=0.8)
+        plt.text(0.1*sigma,1.05*n_min,r'${}\sigma$'.format(x),color=color_dotted,fontsize=16)
+    plt.xlim(left=min(delta),right=0.5)
+    plt.ylim(0.9*min(numerator),1e3)
+    plt.xlabel('inconfidence level $\delta$',fontsize="x-large")
+    plt.ylabel('collected samples $N_i$',fontsize="x-large")
+    plt.xticks([1e-10,1e-8,1e-6,1e-4,1e-2],["$10^{"+str(i)+"}$" for i in np.arange(-10,-1,2)],fontsize="x-large")
+    plt.yticks([1e2,1e3],["$10^{"+str(i)+"}$" for i in np.arange(2,4)],fontsize="x-large")
+    #plt.minorticks_off()
+    plt.savefig("generated_figures/fig2_demo.png",bbox_inches="tight")
