8a4d703ec90e42d27b19fd63014f53aeec1a7c13
Abbas <a.ebadi2013@gmail.com>
2026-01-21 18:01:07 +0100

    hi

--- demo_fig2.py
+++ demo_fig2.py
@@ -11,7 +11,7 @@ from os.path import isdir
 delta = np.logspace(-10,0,1000)
 delta = delta[delta<0.5]
 numerator = N_delta(delta)
-# shadow
+# shadow hi
 
 n_min, n_max = min(numerator), max(numerator)
 #######################################################
